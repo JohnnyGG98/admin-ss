ddb2e5725c66400d375a087ada3efa50bee6988d
Johnny <johnnygar98@hotmail.com>
2019-09-01 15:59:38 -0500

    Iniciando el proyecto

---
+++ src/main/java/com/shopshopista/adminss/AdminSsApplication.java
@@ -0,0 +1,13 @@
+package com.shopshopista.adminss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminSsApplication.class, args);
+	}
+
+}
