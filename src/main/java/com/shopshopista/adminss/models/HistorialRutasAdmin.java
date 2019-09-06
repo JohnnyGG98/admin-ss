de8921d9990fe103b356dd192aed0850d6365540
Andresn97 <46432464+Andresn97@users.noreply.github.com>
2019-09-05 20:10:04 -0500

    Nuevo modelo

---
+++ src/main/java/com/shopshopista/adminss/models/HistorialRutasAdmin.java
@@ -0,0 +1,11 @@
+
+package com.shopshopista.adminss.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "HistorialRutasAdmin")
+public class HistorialRutasAdmin {
+    
+}
